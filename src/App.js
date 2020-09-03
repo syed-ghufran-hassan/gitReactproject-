4f664b4bb9cfdc2c44b193f362b9b8df4ff80dea
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2020-09-03 22:16:42 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
 
 function App() {
   return (
@@ -21,6 +23,19 @@ function App() {
       </header>
     </div>
   );
+  class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
