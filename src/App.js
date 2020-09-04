f0ccbac341fe81736d01965eb869c72ac8d01171
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2020-09-04 10:50:55 +0500

    components part 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 
 
 function App() {
@@ -37,5 +38,6 @@ function App() {
     }
   }
 }
+
 
 export default App;
